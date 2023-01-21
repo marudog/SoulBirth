84f8b5a2cf4d3a24312d7181f3e82200ed1a8e0c
marudog <marulia@protonmail.ch>
2023-01-21 12:37:51 +0900

    add Dhana

--- js/birthdate.js
+++ js/birthdate.js
@@ -12,7 +12,8 @@ var BirthDate = Array
         new Date (Year, 10, 4) ,
         new Date (Year, 2, 2) ,
         new Date (Year, 9, 10) ,
-        new Date (Year, 3, 15)
+        new Date (Year, 3, 15) ,
+        new Date (Year, 11, 14)
     )
 
 for (i = 0; i < BirthDate.length; i++) {
